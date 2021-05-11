f5800aa8300398ae88136e730380abbab0036e7e
gg-dev-05 <garvitgalgat@gmail.com>
2021-05-12 01:51:14 +0530

    all test cases

--- test.py
+++ test.py
@@ -1,18 +1,8 @@
 import unittest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from secret import email, password, rasta, secret_key, email_user, password_user
 
-# from tests.admin import FlaskAdminTestCase 
+from tests.admin import FlaskAdminTestCase 
 from tests.user import FlaskUserTestCase
-# from tests.clubs import FlaskClubsTestCase 
-# class FlaskTestCase(unittest.TestCase):               
-
-
-	
+from tests.clubs import FlaskClubsTestCase 
 
 if __name__ == '__main__':
 	unittest.main()
